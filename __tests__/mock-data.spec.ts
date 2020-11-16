e83e2f702393f38abf6653a072003c7f9c01a3e4
步茗 <jiazhe.wjz@antfin.com>
2020-11-16 16:47:25 +0800

    fix: test case

--- __tests__/mock-data.spec.ts
+++ __tests__/mock-data.spec.ts
@@ -1,26 +1,17 @@
 import { mock } from '../src/mock-data';
 
 test('one enum', () => {
-  expect(mock([{ type: 'enum', values: ['A', 'B', 'C'], name: 'c1' }], 10).map(item => item.c1)).toEqual([
-    'A',
-    'B',
-    'C',
-    'A',
-    'B',
-    'C',
-    'A',
-    'B',
-    'C',
-    'A',
-  ]);
+  expect(
+    mock([{ type: 'enum', values: ['A', 'B', 'C'], name: 'c1', distribution: 'cartesian' }], 10).map(item => item.c1)
+  ).toEqual(['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A']);
 });
 
 test('two enum', () => {
   expect(
     mock(
       [
-        { type: 'enum', values: ['A', 'B', 'C'], name: 'c1' },
-        { type: 'enum', values: ['a', 'b', 'c'], name: 'c2' },
+        { type: 'enum', values: ['A', 'B', 'C'], name: 'c1', distribution: 'cartesian' },
+        { type: 'enum', values: ['a', 'b', 'c'], name: 'c2', distribution: 'cartesian' },
       ],
       10
     ).map(item => `${item.c1}${item.c2}`)
